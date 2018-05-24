fae4fc67b37999c77a1355169509df89d215af8d
Oliver Ball <olivermartinball@gmail.com>
2018-05-24 15:32:15 -0400

    Deleted silly insult. Corrected SudokuGame.from_file. Fixed SudokuGame.init. Commented out method_missing.

--- sudoku.rb
+++ sudoku.rb
@@ -1,26 +1,24 @@
 require_relative "board"
 require 'colorize'
 
-puts "Only contractors write code this bad.".yellow
-
 class SudokuGame
   def self.from_file(filename)
-    board = self.from_file(filename)
+    board = Board.from_file(filename)
     self.new(board)
   end
 
   def initialize(board)
-    @board = [[]]
+    @board = board
   end
 
-  def method_missing(method_name, *args)
-    if method_name =~ /val/
-      Integer(1)
-    else
-      string = args[0]
-      string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
-    end
-  end
+  # def method_missing(method_name, *args)
+  #   if method_name =~ /val/
+  #     Integer(1)
+  #   else
+  #     string = args[0]
+  #     string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
+  #   end
+  # end
 
   def get_pos
     pos = nil
@@ -64,7 +62,7 @@ class SudokuGame
   end
 
   def solved?
-    self.solved?
+    @board.solved?
   end
 
   def valid_pos?(pos)
